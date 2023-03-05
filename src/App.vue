706f924990e8a4f60b63c247bca01e58154172b0
lucien144 <honza@lucien144.net>
2023-03-05 08:08:28 +0100

    feat: menus endpoint

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,17 @@ import router from "@/router";
 
 useHead({ titleTemplate: (title?: string) => !title ? 'Movie Surfers' : `${title} - Movie Surfers` });
 const route = useRoute();
-const categories = ref([]);
+const menus = ref([]);
 onMounted(async () => {
 	try {
-		const result = await fetch(`${import.meta.env.VITE_API_URL}/categories`);
-		categories.value = await result.json();
+		const result = await fetch(`${import.meta.env.VITE_API_URL}/menus`);
+		menus.value = await result.json();
 
 		if (result.status !== 200) {
-			throw new Error(categories.value?.message);
+			throw new Error(menus.value?.message);
 		}
 	} catch (e) {
-		categories.value = [];
+		menus.value = [];
 	}
 });
 </script>
@@ -24,19 +24,20 @@ onMounted(async () => {
 <template>
 	<div class="px-8">
 		<header class="container py-16 md:py-32 mx-auto text-center border-b-2 border-slate-800">
-			<div class="flex justify-between items-center">
+			<div class="flex flex-wrap justify-between items-center">
 				<h1 class="text-4xl font-extrabold"><RouterLink to="/">Movie Surfers</RouterLink></h1>
 				<nav>
-					<ul class="flex space-x-16">
-						<template v-for="category in categories" :key="category.id">
-							<li v-if="['Recenze', 'Trailer', 'Kultovky'].includes(category.name)">
+					<ul class="flex space-x-16 mt-16 sm:mt-0">
+						<template v-for="main in menus.main" :key="main.id">
+							<li>
 								<RouterLink
-									:to="`/${category.slug}--${category.id}/`"
+									:to="`${main.url}`"
 									class="underline"
+									:target="main.target"
 									:class="{
-										'font-bold': router.currentRoute.value.fullPath === `/${category.slug}/`
+										'font-bold': router.currentRoute.value.fullPath === main.url
 									}"
-								>{{ category.name }}</RouterLink>
+								>{{ main.title }}</RouterLink>
 							</li>
 						</template>
 					</ul>
